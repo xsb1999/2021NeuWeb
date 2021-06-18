3096c750f6a8a7b2ebe192e9acaff331059fb67d
xsb1999 <18810235625@163.com>
2021-06-19 03:02:59 +0800

    finish tutorial 06 - JSF Validation

--- src/main/java/managedbeans/Project.java
+++ src/main/java/managedbeans/Project.java
@@ -1,6 +1,11 @@
 package managedbeans;
 
+import utils.DateUtils;
+
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
+import java.util.Date;
 
 @ManagedBean
 public class Project {
@@ -9,6 +14,9 @@ public class Project {
     private String description;
     private String type;
     private String language;
+    private Double cost;
+    private Date startDate;
+    private Date finishDate;
 
     public String getTitle() {
         return title;
@@ -50,8 +58,61 @@ public class Project {
         this.language = language;
     }
 
+    public Double getCost() {
+        return cost;
+    }
+
+    public void setCost(Double cost) {
+        this.cost = cost;
+    }
+
+    public Date getStartDate() {
+        if (startDate == null) {
+            startDate = new Date();
+        }
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getFinishDate() {
+        if (finishDate == null) {
+            finishDate = DateUtils.nextDay(getStartDate());
+        }
+        return finishDate;
+    }
+
+    public void setFinishDate(Date finishDate) {
+        this.finishDate = finishDate;
+    }
+
+
+
     public String update(){
-        return "edit_result";
+        String messageText = "";
+        Date today = new Date();
+        if (startDate.after(today)) {
+            messageText += "The start date can not be in the future";
+        }
+        if (finishDate.after(today)) {
+            messageText += ",    "+"the finish date can not be in the future";
+        }
+        if (!startDate.before(finishDate)) {
+            messageText += ",    "+"start date must be before finish date";
+        }
+        if (messageText != "") {
+            startDate = null;
+            finishDate = null;
+            FacesMessage errorMessage = new FacesMessage(messageText);
+            errorMessage.setSeverity(FacesMessage.SEVERITY_ERROR);
+            FacesContext.getCurrentInstance().addMessage(null,
+                    errorMessage);
+            return null;
+        } else {
+            return("edit_result");
+        }
     }
 
 
--- web/js/myProjects.js
+++ web/js/myProjects.js
@@ -1,14 +1,17 @@
-function Project(title, summary, description, type, language){
+function Project(title, summary, description, type, language, cost, start_date, finish_date){
     this.title = title;
     this.summary = summary;
     this.description = description;
     this.type = type;
     this.language = language;
+    this.cost = cost;
+    this.start_date = start_date;
+    this.finish_date = finish_date
 }
 
-var p1 = new Project('RRT','Three types of RRT algorithm','RRT is a path planning method using in autonomous driving based on sampling.It can quickly search the entire state space (the real world) and is biased towards the unexplored space. It is very similar to trees with lots of limbs stretching out at each time.The main working flow is that we start from the start position and protrude some limbs randomly, once the limb of the tree reaches the destination, the path is found successfully.','algorithm','Python');
-var p2 = new Project('HIS','The online hospital system of NEU','It is an online hospital system of NEU, including six functions (registration, return, visit, prescription, delivery, payment).','development','Java');
-var p3 = new Project('APDP','The data processing of the environment data','The mechine detecting the concentration of CO2 in the air will generate lots of raw data, I need to process them, deleting those bad data, calculating many values like the average.','data processing','Python');
-var p4 = new Project('PSO','My improvement of PSO algorithm','I changed some parameters of the function in the original PSO algorithm, like the weight, the number of particles. By the way, the speed of the algorithm is slightly improved after my modification.','algorithm','Python');
+var p1 = new Project('RRT','Three types of RRT algorithm','RRT is a path planning method using in autonomous driving based on sampling.It can quickly search the entire state space (the real world) and is biased towards the unexplored space. It is very similar to trees with lots of limbs stretching out at each time.The main working flow is that we start from the start position and protrude some limbs randomly, once the limb of the tree reaches the destination, the path is found successfully.','algorithm','Python',100.2,"1/1/2020","3/1/2020");
+var p2 = new Project('HIS','The online hospital system of NEU','It is an online hospital system of NEU, including six functions (registration, return, visit, prescription, delivery, payment).','development','Java',80.5, "7/1/2020","8/12/2020");
+var p3 = new Project('APDP','The data processing of the environment data','The mechine detecting the concentration of CO2 in the air will generate lots of raw data, I need to process them, deleting those bad data, calculating many values like the average.','data processing','Python',24.8,"11/11/2020","12/20/2020");
+var p4 = new Project('PSO','My improvement of PSO algorithm','I changed some parameters of the function in the original PSO algorithm, like the weight, the number of particles. By the way, the speed of the algorithm is slightly improved after my modification.','algorithm','Python',325.5,"3/20/2021","5/9/2021");
 
 var myProjects = [p1,p2,p3,p4];
